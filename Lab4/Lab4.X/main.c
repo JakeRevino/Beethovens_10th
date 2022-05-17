c68a39aee5351977988e34175f15279b8ead9d7f
Aishwary Panchal <apanchal@ucsc.edu>
2022-05-16 22:12:15 -0700

    created main and added .c and .h files

---
+++ Lab4/Lab4.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Beethoven's 10th Symphony
+ *
+ * Created on May 16, 2022, 10:10 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(void) {
+
+    return (EXIT_SUCCESS);
+}
+
